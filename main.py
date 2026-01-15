53c357ed782ff7369fa3619bea02fde8f8a64277
Enes Yilmaz <enesyilmaz5157@gmail.com>
2026-01-15 18:29:32 -0500

    exploring dataset

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+import torch
+from torch import nn
+import pandas as pd
+
+df = pd.read_csv("data/loan_data.csv")
+df = df[["loan_status", "person_income", "loan_intent", "loan_percent_income", "credit_score"]]
+print(df.head())
