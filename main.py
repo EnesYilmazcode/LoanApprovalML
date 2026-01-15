18b6787fc91ac1d54afefb1e654b8e0b7a708cfa
Enes Yilmaz <enesyilmaz5157@gmail.com>
2026-01-15 18:38:41 -0500

    added preliminary information

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ import pandas as pd
 
 df = pd.read_csv("data/loan_data.csv")
 df = df[["loan_status", "person_income", "loan_intent", "loan_percent_income", "credit_score"]]
-print(df.head())
+df = pd.get_dummies(df, columns=["loan_intent"])
+
+
+y = torch.tensor(df["loan_status"].values, dtype=torch.float32).reshape((-1, 1))
+x = torch.tensor(df.drop("loan_status", axis=1).values, dtype=torch.float32)
+
+x_mean = x.mean(dim=0)
+x_std = x.std(dim=0)
+
+# normalization
+x = (x - x_mean) / x_std
+
+
